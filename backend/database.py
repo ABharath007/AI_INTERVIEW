b9b0d412ab8cae8419a229624af7ab359d394c6b
ABharath007 <anugubharath07@gmail.com>
2026-05-14 07:12:09 +0530

    full project push

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"), override=True)
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal  = sessionmaker(autocommit = False,
     autoflush = False,
