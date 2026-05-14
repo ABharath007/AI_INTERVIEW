f4879f24b5f28a080edfcea191860f96fa0d7b8c
ABharath007 <anugubharath07@gmail.com>
2026-05-14 07:30:38 +0530

    full project push

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://ai-interview-frontend-xrvs.onrender.com"
+        "https://ai-interview-frontend-xrvs.onrender.com/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
